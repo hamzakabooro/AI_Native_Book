7dfe8836977979f4cfcb37ae4d004c289ce5baa9
hamzakabooro <kaboorohamzakhan32@gmail.com>
2025-12-26 00:18:43 +0500

    Update docusaurus.config.ts

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://ai-native-book.vercel.app',
+  url: 'https://ai-driven-humanoid-robots.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
